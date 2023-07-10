c3c1ee54db4bebcffea7622dedc800ddd649fa11
mh <maxhalverson@live.com>
2023-07-10 16:54:13 -0500

    installed inquirer. added first few prompts for inquirer

---
+++ index.js
@@ -0,0 +1,33 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+// const questions = [];
+inquirer
+    .createPromptModule([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please enter a description of your project',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please enter installation instructions',
+        },
+    ])
+    .then((data))
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
